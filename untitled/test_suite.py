cb92ff109890930d6bd5efada01db063fcd05740
liqi <liqi@liuliu.io>
2018-05-18 21:13:41 +0800

    ceshi

--- untitled/test_suite.py
+++ untitled/test_suite.py
@@ -8,8 +8,7 @@ if __name__  == '__main__':
     tests = [TestMathFunc("test_divide"),TestMathFunc("test_multi"),TestMathFunc("test_add"),TestMathFunc("test_minus")]
     # for i in tests:
     #     suite.addTest(i)
- 
-   suite.addTests(tests)
-   with open('test.html','wb') as f:
+    suite.addTests(tests)
+    with open('test.html','wb') as f:
         runner = HTMLTestRunner(output='/Users/name/jenk/Jenkins-test/untitled')
         runner.run(suite)
